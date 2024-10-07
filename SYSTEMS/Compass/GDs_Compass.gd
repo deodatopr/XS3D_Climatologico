2b809139385fd011d13d44de51408f895cfe3fdf
FernanD029 <carlosfernandoad@outlook.com>
2024-10-07 11:39:25 -0600

    Mejore el estado de la brújula y arregle los marcadores

--- SYSTEMS/Compass/GDs_Compass.gd
+++ SYSTEMS/Compass/GDs_Compass.gd
@@ -1,7 +1,5 @@
 extends Node
 
-@export var Compass : Polygon2D
-@export var Speed : float
 @export var Parent : Control
 @export var Pivot : Node3D
 @export var MarkRef : Array[Node3D]
@@ -9,18 +7,11 @@ extends Node
 
 var Marker = preload("res://SYSTEMS/Compass/Pfb_MarkTest.tscn")
 
-
 func _ready() -> void:
-	SIGNALS.OnCameraRotation.connect(_CameraRotation)
 	for Mark in MarkRef:
 		_SetMarkerPosition(Mark)
 
-func _CameraRotation(_roation : float):
-	Compass.texture_offset.x += _roation * Speed
-	
 func _SetMarkerPosition(NewMark : Node3D):
 	var instance : GDs_CompassMark = Marker.instantiate()
 	instance._initialize(Pivot, NewMark, CompassLenght)
 	Parent.add_child(instance)
-
-	
