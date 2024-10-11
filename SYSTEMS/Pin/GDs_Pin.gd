f73ae09a26430cfc6c20883b02e57e3901008b58
FernanD029 <carlosfernandoad@outlook.com>
2024-10-11 00:59:12 -0600

    Pin 3d para los sitios

---
+++ SYSTEMS/Pin/GDs_Pin.gd
@@ -0,0 +1,21 @@
+@tool
+extends Node
+
+@onready var label_3d: Label3D = $Site_Name
+@onready var sprite_pin: Sprite3D = $'.'
+
+@export var siteName : String = "Site":
+	set(value):
+		siteName = value
+		label_3d.text = value
+		
+@export var pinColor : Color = Color.WHITE:
+	set(value):
+		pinColor = value
+		sprite_pin.modulate = pinColor
+		
+@export var textOffset : Vector2:
+	set(value):
+		textOffset = value
+		label_3d.offset = value
+		
