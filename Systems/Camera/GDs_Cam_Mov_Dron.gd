e2e435ffc079ce80e882906ac79ec8f6b5ad4843
FernanD029 <carlosfernandoad@outlook.com>
2024-10-24 13:46:13 -0600

    cambio rotacion de la camara

--- Systems/Camera/GDs_Cam_Mov_Dron.gd
+++ Systems/Camera/GDs_Cam_Mov_Dron.gd
@@ -17,7 +17,7 @@ var last_pivot_rotation : Vector3
 var return_cam_time_elpased : float = 0
 var last_pitch : float = 0
 var yaw_direction = 1
-var BoostTimeElapsed = 0
+var BoostTimeElapsed = -1.5
 var CursorMovement : Vector2
 var speed01 : float
 
@@ -70,20 +70,22 @@ func _movement(_delta:float):
 
 	var FinalSpeedTurbo : float = camMng.dron_boost
 	FinalSpeedTurbo = camMng.dron_boost if Input.is_action_pressed("3DMove_SpeedBoost") else 0.0
-	
+
 	if inputDir.length() > 0:
 		mov_velocity += (inputDir * camMng.dron_speed * _delta) + mov_velocity.normalized() * (FinalSpeedTurbo * 10)
-		
 		if mov_velocity.length() > camMng.dron_speed_accel_decel:
-			mov_velocity = lerp(mov_velocity.limit_length(camMng.dron_speed_accel_decel), mov_velocity.limit_length(camMng.dron_speed_accel_decel + (FinalSpeedTurbo * 10)), clampf(BoostTimeElapsed, 0, 1))
-			if FinalSpeedTurbo != 0 and BoostTimeElapsed <= 1:
+			mov_velocity = lerp(mov_velocity.limit_length(camMng.dron_speed_accel_decel), mov_velocity.limit_length(camMng.dron_speed_accel_decel + (FinalSpeedTurbo * 10)), 0)#clampf(BoostTimeElapsed, -1.5, .1))
+			if FinalSpeedTurbo != 0 and BoostTimeElapsed <= .1:
 				BoostTimeElapsed += _delta
+			if FinalSpeedTurbo == 0 and BoostTimeElapsed > -1.5:
+				BoostTimeElapsed -= _delta
+		
 	else:
 		if mov_velocity.length() > 0:
 			mov_velocity -= mov_velocity.normalized() * mov_deceleration * _delta
 			if mov_velocity.length() < 0.1:
 				mov_velocity = Vector3.ZERO
-				BoostTimeElapsed = 0
+				BoostTimeElapsed = -1.5
 	
 	speed01 = inverse_lerp(0,camMng.dron_speed + (FinalSpeedTurbo * 10),mov_velocity.length())
 	var targetPosition : Vector3 = pivot.global_position
@@ -100,25 +102,23 @@ func _movement(_delta:float):
 func _rotation(_delta:float):
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		
 		if MouseMotion != null:
 			if MouseMotion.relative.x > 0:
 				yaw_direction = 1
 			else:
 				yaw_direction = -1
-			#CursorMovement += MouseMotion.relative * 3
-			#CursorMovement = clamp(CursorMovement, -(DisplayServer.screen_get_size()*1.0)/2, (DisplayServer.screen_get_size()*1.0)/2)
-			#print(CursorMovement)
-			#_rotHorPivot(CursorMovement.x/(DisplayServer.screen_get_size().x/2) * 10, _delta)
-			#_rotVertCam(CursorMovement.y/(DisplayServer.screen_get_size().y/2) * 10, _delta)
-			_rotHorPivot(MouseMotion.relative.x, _delta)
-			_rotVertCam(MouseMotion.relative.y, _delta)
+			CursorMovement += MouseMotion.relative * 2
+			CursorMovement.y = clampf(CursorMovement.y, -(DisplayServer.screen_get_size()*1.0).y/2, (DisplayServer.screen_get_size()*1.0).y/2)
+			CursorMovement.x = clampf(CursorMovement.x, -(DisplayServer.screen_get_size()*1.0).x/2, (DisplayServer.screen_get_size()*1.0).x/2)
+			_rotHorPivot((CursorMovement.x*2)/DisplayServer.screen_get_size().x * 10, _delta)
+			_rotVertCam((CursorMovement.y*2)/DisplayServer.screen_get_size().y * 10, _delta)
+		else:
+			_rotHorPivot((CursorMovement.x*2)/DisplayServer.screen_get_size().x * 10, _delta)
+			_rotVertCam((CursorMovement.y*2)/DisplayServer.screen_get_size().y * 10, _delta)
 
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT): 
-		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		CursorMovement = Vector2.ZERO
-		pass
 		
 	if Input.is_action_pressed("3DLook_Up") or Input.is_action_pressed("3DLook_Down"):
 		_rotVertCam(-Input.get_axis("3DLook_Down", '3DLook_Up') * 10, _delta)
@@ -133,7 +133,7 @@ func _rotation(_delta:float):
 		pivot.rotation_degrees.y = yaw
 
 func _rotHorPivot(dir:float, _delta:float):
-	yaw -= dir * (camMng.dron_rot_hor_speed * _delta * UTILITIES.GetCurvePoint(camMng.curveAccel, 1.2, _delta, false))
+	yaw -= dir * (camMng.dron_rot_hor_speed * UTILITIES.GetCurvePoint(camMng.curveAccel, 1.2, _delta, false)) * _delta
 	pivot.rotation_degrees.y = yaw
 
 func _rotVertCam(dir:float, _delta:float):
