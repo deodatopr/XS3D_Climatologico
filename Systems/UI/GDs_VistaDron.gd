0be506300a650ebac1ddfd690d9861db6ad08999
EnriqueLara <43079987+EnriqueLara@users.noreply.github.com>
2024-10-28 08:22:06 -0600

    Fix Vista Dron

--- Systems/UI/GDs_VistaDron.gd
+++ Systems/UI/GDs_VistaDron.gd
@@ -6,8 +6,7 @@ extends Control
 @export var lblVelocidad:Label
 @export var lblRotacion:Label
 @export_group("Lineas")
-@export var topLines:Control
-@export var bottomLines:Control
+@export var centerLine:Control
 @export var LeftLines:Control
 @export var RightLines:Control
 
@@ -27,19 +26,17 @@ var initialRightLinesPosY: float
 
 
 func Initialize():
-	pass
-	#initialTopLinesPosX = topLines.position.x
-	#initialBottomLinesPosX = bottomLines.position.x
-	#initialLeftLinesPosY = LeftLines.position.y
-	#initialRightLinesPosY = RightLines.position.y
+	initialTopLinesPosX = centerLine.position.x
+	initialLeftLinesPosY = LeftLines.position.y
+	initialRightLinesPosY = RightLines.position.y
 	
-	#minimap.cam_Manager = cam_manager
-	#minimap.mark_target = worldMark
-	#minimap.map = map
-	#minimap.Initialize()
-	#
-	#compass.PinPos = worldMark
-	#compass.Initialize(cam_manager)
+	minimap.cam_Manager = cam_manager
+	minimap.mark_target = worldMark
+	minimap.map = map
+	minimap.Initialize()
+	
+	compass.PinPos = worldMark
+	compass.Initialize(cam_manager)
 
 func _process(_delta):
 	lblAltura.text = UTILITIES.FormatAltura(CAM.height)
@@ -47,11 +44,10 @@ func _process(_delta):
 	lblVelocidad.text = UTILITIES.FormatVelocidad(CAM.speed)
 	lblRotacion.text = UTILITIES.FormatRotacionXY(CAM.rotation)
 
-#func _physics_process(_delta):
+func _physics_process(_delta):
 	#TODO agregar posicion de camara a las lineas horizontales 
-	#topLines.position.x = initialTopLinesPosX +(CAM.rotation.y * 3) 
-	#bottomLines.position.x = initialBottomLinesPosX +(CAM.rotation.y * 3) 
-	#
-	#LeftLines.position.y = initialLeftLinesPosY + (CAM.rotation.x * 2) + CAM.height
-	#RightLines.position.y = initialRightLinesPosY + (CAM.rotation.x * 2) + CAM.height
+	centerLine.position.x = initialTopLinesPosX +(CAM.rotation.y * 3)
+	
+	LeftLines.position.y = initialLeftLinesPosY + (CAM.rotation.x * 2) + CAM.height
+	RightLines.position.y = initialRightLinesPosY + (CAM.rotation.x * 2) + CAM.height
 	
