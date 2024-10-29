751e72b73ea95db4c7b3d595c61c48ee7deebd3a
EnriqueLara <43079987+EnriqueLara@users.noreply.github.com>
2024-10-29 11:00:31 -0600

    Fix lineas por input en lugar de position

--- Systems/UI/GDs_LineIndicators.gd
+++ Systems/UI/GDs_LineIndicators.gd
@@ -8,6 +8,8 @@ extends Control
 
 var initialPosX:float
 var initialPosY:float
+var posX:float
+var posY:float
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	initialPosX = position.x
@@ -16,18 +18,31 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
+	
+	if Input.is_action_pressed('3DMove_Left'):
+		posX += 0.2
+	if Input.is_action_pressed('3DMove_Right'):
+		posX -= 0.2
+	if Input.is_action_pressed('3DMove_Forward'):
+		posY += 0.2
+	if Input.is_action_pressed('3DMove_Backward'):
+		posY -= 0.2
+	
 	if vertical:
 		if useCamRotation:
 			if useXaxis:
 				position.y = initialPosY + (int(CAM.rotation.x*3) % width)
 			else:
 				position.y = initialPosY + (int(CAM.rotation.y*3) % width)
+		#POSICION VERTICAL
 		else:
-			position.y = initialPosY + (int(CAM.position.z) % width)
+			position.y = initialPosY + (int(posY) % width)
 	else:
 		if useCamRotation:
 			position.x = initialPosX + (int(CAM.rotation.y*3) % width)
+		#POSICION HORIZONTAL
 		else:
 			#FIXME cambiar por inputs en lugar dee position
-			position.x = initialPosX + (int(CAM.position.x) % width)
+			
+			position.x = initialPosX + (int(posX) % width)
 	
