a0b2c865250f5058aca8f6fb0a071219a36ad610
EnriqueLara <43079987+EnriqueLara@users.noreply.github.com>
2024-10-30 14:15:24 -0600

    Fixes UI, WIP inyeccion de datos

--- Systems/Menus/Perfiles/GDs_Perf_Sitio.gd
+++ Systems/Menus/Perfiles/GDs_Perf_Sitio.gd
@@ -7,9 +7,35 @@ extends Control
 @export var scaleScale:Vector2
 @export var blurBG:Control
 @export_group("Data Refresh")
+@export_subgroup("Color, ID & Nombre")
 @export var frame:Control
 @export var frameNombre:Control
 @export var patch:Control
+@export var id:Label
+@export var nombre:Label
+@export var estado:Label
+@export_subgroup("Senales")
+@export var senal:Control
+@export var UTR:Control
+@export var OnColor:Color
+@export var OffColor:Color
+@export_subgroup("Nivel")
+@export var nivel:Label
+@export var nivelBg:Control
+@export var nivelNorm:Label
+@export var nivelPrev:Label
+@export var nivelCrit:Label
+@export var ColorNorm:Color
+@export var ColorPrev:Color
+@export var ColorCrit:Color
+@export_subgroup("Datos")
+@export var precipitacion:Label
+@export var humedad:Label
+@export var evaporacion:Label
+@export var temp:Label
+@export var presion:Label
+@export var viento:Label
+
 
 
 signal OnSitioPressed 
@@ -32,7 +58,36 @@ func DataRefresh(_estacion: GDs_Data_Estacion):
 	frame.self_modulate = _estacion.color
 	frameNombre.self_modulate = _estacion.color
 	patch.self_modulate = _estacion.color
-
+	id.text = str(_estacion.id)
+	nombre.text = _estacion.nombre
+	estado.text = UTILITIES.FormatEstado(_estacion.estado)
+	
+	#Senales
+	if _estacion.enlace:
+		senal.self_modulate = OnColor
+	else:
+		senal.self_modulate = OffColor
+	if _estacion.disp_utr:
+		UTR.self_modulate = OnColor
+	else:
+		UTR.self_modulate = OffColor
+	
+	#nivel
+	nivel.text = UTILITIES.FormatNivel(_estacion.nivel)
+	nivelPrev.text = UTILITIES.FormatNivel(_estacion.nivelPrev)
+	nivelCrit.text = UTILITIES.FormatNivel(_estacion.nivelCrit)
+	
+	nivelBg.self_modulate = ColorNorm
+	if _estacion.nivel > _estacion.nivelPrev: nivelBg.self_modulate = ColorPrev
+	if _estacion.nivel > _estacion.nivelCrit: nivelBg.self_modulate = ColorCrit
+	
+	#Datos
+	precipitacion.text = UTILITIES.FormatPptn_pluvial(_estacion.pptn_pluvial)
+	humedad.text = UTILITIES.FormatHumedad(_estacion.humedad)
+	evaporacion.text = UTILITIES.FormatEvaporacion(_estacion.evaporacion)
+	temp.text = UTILITIES.FormatTemperatura(_estacion.temperatura)
+	presion.text = UTILITIES.FormatPresion(_estacion.temperatura)#TODO cambiar por presion
+	viento.text = UTILITIES.FormatIntensidadViento(_estacion.intsdad_viento)#TODO sistema para que regrese N S E O
 
 func OnBtnPressed():
 	OnSitioPressed.emit()
