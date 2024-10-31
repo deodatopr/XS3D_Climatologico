9bc9f20263dec7ac958063bdeeb4717d2ee69374
EnriqueLara <43079987+EnriqueLara@users.noreply.github.com>
2024-10-31 13:06:21 -0600

    Sonido drones

--- Systems/Audio/GDs_Audio_DroneSky.gd
+++ Systems/Audio/GDs_Audio_DroneSky.gd
@@ -1,34 +1,54 @@
 extends Node
 
+@export_group("Moving")
 @export var sndMoving: AudioStreamPlayer
-@export var sndMovingVolume: float = -5.0
+@export var sndMovingVolume:float = 7.0
 @export var sndMovingPitch: float = 2.0
-@export var sndWind: AudioStreamPlayer
+@export var sndMovingTurboPitch: float = 2.0
+@export_group("Camera FOV")
 @export var sndCameraFOV: AudioStreamPlayer
+@export var sndCameraFOVVolume: float = 10.0
+@export var sndCameraFOVPitch: float = 0.7
+@export_group("CameraRot")
 @export var sndCameraRot: AudioStreamPlayer
+@export var sndCameraRotVolume: float = 7
+@export var sndCameraRotPitch: float = 0.5
+@export_group("Wind")
+@export var sndWind: AudioStreamPlayer
+@export var sndWindVolume: float = -5
+@export var sndWindPitch: float = 1
 
 
-var tween:Tween
+var tweenMoving:Tween
+var tweenCamFOV:Tween
 var sndCameraVol:float
+var isBoosting = false
 
 func _ready():
 	SIGNALS.OnCameraChangedMode.connect(OnChangeDrone)
-	sndCameraVol = sndCameraFOV.volume_db
-
 
 func _process(_delta):
 	if APPSTATE.camMode == ENUMS.Cam_Mode.sky:
+#region Moving
 		if Input.is_action_pressed("3DMove_Forward") or Input.is_action_pressed("3DMove_Backward") or Input.is_action_pressed("3DMove_Left") or Input.is_action_pressed("3DMove_Right"):
-		#if CAM.speed != 0:
-			if not sndMoving.playing:
-				FadeIn(sndMoving, sndMovingVolume,sndMovingPitch)
-			sndWind.pitch_scale = 1.5
-			
+			if Input.is_action_pressed("3DMove_SpeedBoost") and not isBoosting:
+				isBoosting = true
+				MovingFadeIn()
+				sndWind.pitch_scale = 2
+			elif not Input.is_action_pressed("3DMove_SpeedBoost") and isBoosting: 
+				isBoosting = false
+				MovingFadeIn()
+				sndWind.pitch_scale = 1.5
+			else:
+				if not sndMoving.playing:
+					isBoosting = false
+					MovingFadeIn()
+					sndWind.pitch_scale = 1.5
 		else:
 			if sndMoving.playing:
-				FadeOut(sndMoving,-40,1)
-			sndWind.pitch_scale = 1
-	
+				MovingFadeOut()
+				sndWind.pitch_scale = 1
+#endregion
 #region CameraFOV
 		if Input.is_action_pressed("3DMove_Height_-") and Input.is_action_pressed("3DMove_Height_+"):
 			if sndCameraFOV.playing:
@@ -43,38 +63,50 @@ func _process(_delta):
 			if sndCameraFOV.playing:
 				CameraFadeOut()
 #endregion
+#region Camera Rotation
+
+		sndCameraRot.pitch_scale = sndCameraRotPitch
+		sndCameraRot.volume_db = sndCameraRotVolume
 		if CAM.isRotating:
 			sndCameraRot.play()
 		else:
 			sndCameraRot.stop()
+		
+		sndWind.volume_db = sndWindVolume
+		sndWind.pitch_scale = sndWindPitch
+#endregion
 
  
-func FadeIn(_audio:AudioStreamPlayer,targetVol:float, targetPitch:float):
-	_audio.play()
-	tween = create_tween()
-	tween.tween_property(_audio,"volume_db",targetVol,0.5)
-	tween.parallel().tween_property(_audio,"pitch_scale",targetPitch,1.5)
+func MovingFadeIn():
+	sndMoving.play()
+	if tweenMoving: tweenMoving.kill()
+	tweenMoving = create_tween()
+	tweenMoving.tween_property(sndMoving,"volume_db",sndMovingVolume,0.2)
+	tweenMoving.parallel().tween_property(sndMoving,"pitch_scale",sndMovingPitch,0.2)
 
-func FadeOut(_audio:AudioStreamPlayer,targetVol:float, targetPitch:float):
-	tween = create_tween()
-	tween.tween_property(_audio,"volume_db",targetVol,1)
-	tween.parallel().tween_property(_audio,"pitch_scale",targetPitch,1)
-	await tween.finished
-	_audio.stop()
+func MovingFadeOut():
+	if tweenMoving: tweenMoving.kill()
+	tweenMoving = create_tween()
+	tweenMoving.tween_property(sndMoving,"volume_db",-10,0.5)
+	tweenMoving.parallel().tween_property(sndMoving,"pitch_scale",1,0.5)
+	await tweenMoving.finished
+	sndMoving.stop()
 
+#region Camera FOV
 func CameraFadeIn():
 	sndCameraFOV.play()
-	tween = create_tween()
-	tween.tween_property(sndCameraFOV,"volume_db",10,0.1)
-	tween.parallel().tween_property(sndCameraFOV,"pitch_scale",0.7,0.1)
+	tweenCamFOV = create_tween()
+	tweenCamFOV.tween_property(sndCameraFOV,"volume_db",sndCameraFOVVolume,0.1)
+	tweenCamFOV.parallel().tween_property(sndCameraFOV,"pitch_scale",sndCameraFOVPitch,0.1)
 
 func CameraFadeOut():
-	tween = create_tween()
-	tween.tween_property(sndCameraFOV,"volume_db",-10,0.1)
-	tween.parallel().tween_property(sndCameraFOV,"pitch_scale",0.5,0.1)
-	await tween.finished
+	tweenCamFOV = create_tween()
+	tweenCamFOV.tween_property(sndCameraFOV,"volume_db",-10,0.1)
+	tweenCamFOV.parallel().tween_property(sndCameraFOV,"pitch_scale",sndCameraFOVPitch - 0.4,0.1)
+	await tweenCamFOV.finished
 	sndCameraFOV.stop()
 	pass
+#endregion
 
 
 func OnChangeDrone(_mode:int):
