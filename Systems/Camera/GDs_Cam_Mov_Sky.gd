ba01fed09fb168ea31f738c3929fbfc293b9a86d
Paniagua96 <44847642+Paniagua96@users.noreply.github.com>
2024-10-31 23:31:50 -0600

    Cam Drones

--- Systems/Camera/GDs_Cam_Mov_Sky.gd
+++ Systems/Camera/GDs_Cam_Mov_Sky.gd
@@ -2,13 +2,8 @@ class_name GDs_Cam_Mov_Sky extends Node
 
 var camMng : GDs_Cam_Manager
 var cam : Camera3D
-var pivot_cam : Node3D
+var pivot : Node3D
 
-var axisLeftVert : float
-var axisLeftHor : float
-
-var axisRightVert : float
-var axisRightHor : float
 
 #FOV
 var fov_current : float
@@ -16,11 +11,9 @@ var fov_01 : float
 var fov_lensDistIntensity : float
 
 #Movement
-var mov_speed : float
+var mov_axis : Vector2
 var mov_speed01 : float = 0
-var mov_speed_boost : float
 var mov_curv01 : float
-var mov_max_acceleration : float
 var mov_velocity : Vector3
 var mov_isMoving : bool
 var mov_isPressingMove : bool
@@ -31,85 +24,68 @@ var mov_isInsideBoundings : bool
 var mov_lastBounding01 : float
 
 #Rotation
-var rotHor_yaw : float = 0.0
-var rotHor_yaw_direction : float = 1
-var rotHor_cursorMovement : Vector2
-var rotHor_isRotating : bool
-var rotHor_velocity : Vector3
-var MouseMotion : InputEvent
+var rot_axis : float
+var rot_hor : float
+var rot_cursorMovement : Vector2
+var rot_velocity : Vector3
+var rot_lastRotDir : float
+var rot_lastRot : float
+var rot_lastMousePos01 : float
+var mouseMotion : InputEvent
 
 # Misc
 var signalUpdateWasEmitted : bool
 var curvEvaluateSpeed : float = .5
 var curvPoint : float
 
-const ROTHOR_THRESHOLD : float = 0.8
-
 func Initialize(_camMng : GDs_Cam_Manager):
 	camMng = _camMng
 	cam = camMng.sky_cam
-	pivot_cam = camMng.sky_pivot
+	pivot = camMng.sky_pivot
 	mov_speed01 = 0
 	
 	UpdateCamConfig()
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		MouseMotion = event
+		mouseMotion = event
 	else:
-		MouseMotion = null
+		mouseMotion = null
 	
 func SetCamera():
 	cam.current = true
-	cam.global_position.y = camMng.sky_height
-	cam.fov = camMng.sky_zoom_out
-	cam.rotation.x = deg_to_rad(-80)
-	mov_velocity = Vector3.ZERO
-	mov_curv01 = 0
+	cam.fov = camMng.sky_zoom_out	
+	pivot.position.y = camMng.sky_height
 	
+	mov_velocity = Vector3.ZERO
+	mov_curv01 = 0	
+	rot_hor = pivot.rotation.y
 	fov_current = cam.fov
+	
 	_SetLensDistorsion(fov_current)
 
 func UpdateCamConfig():
 	cam.global_position.y = camMng.sky_height
 	cam.fov = camMng.sky_zoom_out
 	
-	#Movement
-	mov_speed = camMng.sky_speed
-	mov_max_acceleration = camMng.sky_speed
-	mov_speed_boost = camMng.sky_turbo
-	
 func _physics_process(delta):
 	_Movement(delta)
-	_Fov(delta)
 	_Rotation(delta)
+	_Fov(delta)
 	
 	#Check boundings map01
-	mov_isInsideBoundings = camMng.CheckMapBoundings(pivot_cam)
-	
-	if (mov_isMoving or rotHor_isRotating) and not signalUpdateWasEmitted:
-		SIGNALS.OnCameraUpdate.emit(true)
-		signalUpdateWasEmitted = true
-	elif (not mov_isMoving and not rotHor_isRotating and mov_velocity.length() == 0) and signalUpdateWasEmitted:
-		SIGNALS.OnCameraUpdate.emit(false)
-		signalUpdateWasEmitted = false
+	mov_isInsideBoundings = camMng.CheckMapBoundings(pivot)
 
 func _Movement(_delta: float):
-	var inputDir = Vector3.ZERO
+	mov_axis = Input.get_vector('3DMove_Left','3DMove_Right','3DMove_Backward','3DMove_Forward')	
+	var isPressingTurbo : bool = Input.is_action_pressed('3DMove_SpeedBoost') 
+	var inputDir : Vector3 = pivot.global_basis * Vector3(-mov_axis.x,mov_axis.y,0).normalized()
 	
-	axisLeftVert = Input.get_axis('3DMove_Backward', '3DMove_Forward')
-	axisLeftHor = Input.get_axis('3DMove_Left', '3DMove_Right')
-	
-	if axisLeftVert != 0:
-		inputDir += pivot_cam.basis.z * axisLeftVert
-		
-	if axisLeftHor != 0:
-		inputDir += pivot_cam.basis.x * -axisLeftHor
-		
-	mov_isPressingMove = inputDir.length() > 0
-	inputDir = inputDir.normalized()
+	mov_isPressingMove = mov_axis.length() > 0
 
 	if mov_isPressingMove:
+		#Acceleration
+		
 		if mov_curv01 < 1:
 			#Calculate curve acc
 			mov_curv01 += curvEvaluateSpeed * _delta
@@ -117,11 +93,13 @@ func _Movement(_delta: float):
 			curvPoint = camMng.curveMovement.sample(mov_curv01)
 		
 		#Calculate velocity to move
-		mov_velocity += inputDir * curvPoint * _delta
+		mov_velocity += inputDir * camMng.sky_speed * curvPoint * _delta
 		@warning_ignore('incompatible_ternary')
-		mov_limitSpeed = mov_speed * (mov_speed_boost if Input.is_action_pressed('3DMove_SpeedBoost') else 1)
+		mov_limitSpeed = camMng.sky_speed * (camMng.sky_turbo if isPressingTurbo else 1)
 		mov_velocity = mov_velocity.limit_length(mov_limitSpeed)
 	elif not mov_isPressingMove and mov_velocity.length() > 0:
+		#Deceleration
+		
 		#Calculate curve dec
 		mov_curv01 -= curvEvaluateSpeed * _delta
 		mov_curv01 = clampf(mov_curv01,0,1)
@@ -145,46 +123,42 @@ func _Movement(_delta: float):
 	mov_velocity = mov_velocity * decreaseSpeedByLimits
 	
 	#Save mov_speed01 to send it to UI
-	var fixSpeed : float =  inverse_lerp(0,mov_speed * mov_speed_boost,mov_velocity.length())
-	var slowSpeed : float = lerpf(mov_lastSpeed01,fixSpeed, .2)
-	@warning_ignore('incompatible_ternary')
-	mov_speed01 = clampf(slowSpeed,0,1 if Input.is_action_pressed('3DMove_SpeedBoost') else .7)
+	var fixSpeed : float =  inverse_lerp(0,camMng.sky_speed * camMng.sky_turbo,mov_velocity.length())
+	var slowSpeed : float = lerpf(mov_lastSpeed01,fixSpeed, 10 * _delta)
+	mov_speed01 = clampf(slowSpeed,0, 1)
 	mov_lastSpeed01 = mov_speed01
 	
 	#Apply movement
 	if mov_velocity.length() > 0:
+		mov_velocity.y = 0
+		pivot.global_position += mov_velocity
 		mov_lastVelocity = mov_velocity
-		mov_velocity = lerp(mov_lastVelocity, mov_velocity, 20)
-		pivot_cam.global_position += mov_velocity
 
 func _Rotation(_delta:float):
+	#Detect input controller
+	rot_axis = Input.get_axis('3DLook_Left','3DLook_Right')
+	var dir : float = signf(rot_axis)
+	
+	#Detect input mouse
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		if MouseMotion != null:
-			if MouseMotion.relative.x > 0:
-				rotHor_yaw_direction = 1
-			else:
-				rotHor_yaw_direction = -1
-			rotHor_cursorMovement += MouseMotion.relative * 2
-			rotHor_cursorMovement.y = clampf(rotHor_cursorMovement.y, -(DisplayServer.screen_get_size()*1.0).y/2, (DisplayServer.screen_get_size()*1.0).y/2)
-			rotHor_cursorMovement.x = clampf(rotHor_cursorMovement.x, -(DisplayServer.screen_get_size()*1.0).x/2, (DisplayServer.screen_get_size()*1.0).x/2)
-			_RotHorPivot((rotHor_cursorMovement.x*2)/DisplayServer.screen_get_size().x * 10,1 ,_delta)
-		else:
-			_RotHorPivot((rotHor_cursorMovement.x*2)/DisplayServer.screen_get_size().x * 10,1, _delta)
+		if mouseMotion != null:
+			var fixedMouseMotion : Vector2 = mouseMotion.relative
+			dir = signf(mouseMotion.relative.x)
 
-	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT): 
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		rotHor_cursorMovement = Vector2.ZERO
-		
-	axisRightHor = Input.get_axis('3DLook_Left','3DLook_Right')
-	if axisRightHor != 0:
-		_RotHorPivot(axisRightHor, 5, _delta)
+	CAM.isRotating = dir != 0
+	dir = lerp(rot_lastRotDir,dir,5 *_delta)
 	
-	if MouseMotion:
-		CAM.isRotating = (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and MouseMotion.relative.x != 0) or axisRightHor != 0
+	#Apply rot
+	_rotation_hor(dir,_delta)
+
+	rot_lastRotDir = dir
+	rot_lastRot = pivot.rotation.y
 	
-func _RotHorPivot(_dir:float, _factorSpeed : float, _delta:float):
-	rotHor_yaw -= _dir * camMng.sky_rot_speed * _factorSpeed  * _delta
-	pivot_cam.rotation_degrees.y = rotHor_yaw
+func _rotation_hor(_dir:float, _delta:float):
+	rot_hor -= _dir * camMng.sky_rot_speed  * _delta
+	var targetAngle : float = rot_hor
+	var smoothTarget : float = lerp_angle(rot_lastRot,targetAngle,15 * _delta)
+	pivot.rotation.y = smoothTarget
 
 func _Fov(_delta : float):
 	if Input.is_action_pressed("3DLook_Fov_+"):
