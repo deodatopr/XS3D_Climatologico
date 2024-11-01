dbdf489151864e7335bf37fc75a2cc71081b5408
Paniagua96 <44847642+Paniagua96@users.noreply.github.com>
2024-11-01 10:17:42 -0600

    Brujula screen funcionando

--- Systems/Tool_TodoTasks.gd
+++ Systems/Tool_TodoTasks.gd
@@ -1,31 +1,16 @@
-#TODO:
+#BUG: Si estas en un modo, está perdiendo señal y te cambias se queda con UI de perdiendo señal
 
+#TODO:
 #===========================================
 #	Pendientes Progra
 #===========================================
-
-# Gatillos izq - turbo
-# Gatillos der - zoom
-# Boton arriba - cambiar
-# Boton izq - 
-
-# Boton
 
 #	[ Pani ]
 # Armado flujo completo (orquestador, datos,etc)
 # Cambio entre sitios
 
-#Splash virtual wave y xsoft (borrar le otro)
-
 #Cam fly
-# - Variable para ver abajo a cuantos grados ves
-# - Umbral rotaciones XY
-
 # - NavMesh que si sube por altura despues baje, si estabas en el suelo
-
-# - Dron abajo DOF near
-	# Limite abajo faltando 5 o 10 lo active
-# - Cierta altura de acuerdo a navMesh prender DOF
 # - Trigger cercano a sitio aumentar DOF
 # a CIERTA DIST APAGAR/PRENDER AO
 
@@ -39,18 +24,13 @@
 
 # Random  noise clouds shadows
 
-# Abrir por primera vez guia de input y opcion para ya no mostrar con boton menos 
 # Parpadear opcion de menu guia
 # Tutorial guiado cuando aparece home (primero como que se presiona home y luego se prenda guia)
 # Endpoint graficadora
-# Delta mov dron eliminar uno de los dos
 
 #===========================================
 #	Pendientes extra
 #===========================================
 
 #	Traducir los grados en la dirección del viento (ej. 90 = N, 45 = E)
-#	Cambio modo
-# 		- Cortina
-# 		- Glich
 #	Tomar screenshots
