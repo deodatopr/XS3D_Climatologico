538225fe909c411b2738cbf489263ba0b44851d5
EnriqueLara <43079987+EnriqueLara@users.noreply.github.com>
2024-11-13 11:55:12 -0600

    INyeccion del color menos saturado a la Baliza

--- Systems/Pin/PuntoDeInteres/GDs_PuntosDeInteres.gd
+++ Systems/Pin/PuntoDeInteres/GDs_PuntosDeInteres.gd
@@ -5,6 +5,9 @@ extends Node
 @export var camSky: Camera3D
 @export_group("Refs Internas")
 @export var canvas: CanvasLayer
+@export_group("Light Beam")
+@export var mat_LightBeam: StandardMaterial3D
+
 
 var PDIs: Array[Node3D]
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +15,10 @@ func _ready():
 	for child in get_children():
 		if child is Node3D and child.visible:
 			PDIs.append(child)
+	
+	var beamColor : Color = APPSTATE.currntSitio.color
+	beamColor.s = 0.2
+	mat_LightBeam.albedo_color = beamColor
 	
 	for pdi in PDIs:
 		var uiPDI = pfbUIPin.instantiate() as GDs_PdI_UIPin
