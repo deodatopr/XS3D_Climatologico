4a46c43b5844284dded9257fa8788291a1c0f8a6
Paniagua96 <44847642+Paniagua96@users.noreply.github.com>
2024-12-10 14:08:57 -0600

    Update

--- Systems/Tool_TodoTasks.gd
+++ Systems/Tool_TodoTasks.gd
@@ -4,8 +4,9 @@
 #	Pendientes Progra
 #===========================================
 
-#Checar puntos de interes, sonidos agua, pin del sitio (Sitio 1)
-#Acomodar puntos de interes, sonidos agua, pin del sitio (Sitios 2 y 3)
+# Checar puntos de interes, sonidos agua, pin del sitio (Sitio 1)
+# Acomodar puntos de interes, sonidos agua, pin del sitio (Sitios 2 y 3)
+# Revisar navegacion con dron fly en (Sitios 1,2 y 3)
 
 #===========================================
 #	TAREAS EXTRA
